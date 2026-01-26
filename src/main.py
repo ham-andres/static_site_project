571e356de0e189449461ec80e3b250bb7cc2fdf8
Hamandres <asd.ray6@gmail.com>
2026-01-26 17:34:46 +0000

    initial phase

---
+++ src/main.py
@@ -0,0 +1,11 @@
+from textnode import TextNode,TextType
+
+def main():
+    node1 = TextNode("Boot.dev",TextType.LINK,"https://www.boot.dev")
+    print(node1)
+
+
+
+
+if __name__ == "__main__":
+    main()
