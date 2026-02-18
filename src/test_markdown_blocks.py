59264ccc7897506123215bf1293b3fe88d4fb458
hamandres <hamandres@Aurelius.lan>
2026-02-18 23:27:40 +0530

    block type

---
+++ src/test_markdown_blocks.py
@@ -0,0 +1,59 @@
+import unittest
+from markdown_blocks import markdown_to_blocks, block_to_block_type, BlockType
+
+class test_markdown_block(unittest.TestCase):
+        def test_markdown_to_blocks(self):
+            md = """
+This is **bolded** paragraph
+
+This is another paragraph with _italic_ text and `code` here
+This is the same paragraph on a new line
+
+- This is a list
+- with items
+"""
+            blocks = markdown_to_blocks(md)
+            self.assertEqual(
+                blocks,
+                [
+                    "This is **bolded** paragraph",
+                    "This is another paragraph with _italic_ text and `code` here\nThis is the same paragraph on a new line",
+                    "- This is a list\n- with items",
+                ],
+            )
+
+        def test_markdown_to_blocks_newlines(self):
+            md = """
+This is **bolded** paragraph
+
+
+
+
+This is another paragraph with _italic_ text and `code` here
+This is the same paragraph on a new line
+
+- This is a list
+- with items
+"""
+            blocks = markdown_to_blocks(md)
+            self.assertEqual(
+                blocks,
+                [
+                    "This is **bolded** paragraph",
+                    "This is another paragraph with _italic_ text and `code` here\nThis is the same paragraph on a new line",
+                    "- This is a list\n- with items",
+                ],
+            )
+
+
+        def test_block_to_block_type(self):
+            block = "# heading"
+            self.assertEqual(block_to_block_type(block), BlockType.HEADING)
+
+            block = "1. first\n2. second"
+            self.assertEqual(block_to_block_type(block), BlockType.ORDEREDLIST)
+
+
+
+if __name__ == "__main__":
+    unittest.main()
