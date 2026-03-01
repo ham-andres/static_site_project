be700dc7a27e93dacb50db5c7d54eb8bb7874500
hamandres <hamandres@Aurelius.lan>
2026-03-01 13:16:02 +0530

    copy construct

--- src/main.py
+++ src/main.py
@@ -1,11 +1,37 @@
-from textnode import TextNode,TextType
+import os
+import shutil
+
+
+def copy_file_recursive(source_dir, dest_dir):
+    if not os.path.exists(dest_dir):
+        os.mkdir(dest_dir)
+
+    for item in os.listdir(source_dir):
+        src_path = os.path.join(source_dir,item)
+        dst_path = os.path.join(dest_dir, item)
+        print(f" source path: {src_path}")
+        print(f"dest path: {dst_path}")
+        
+        if os.path.isfile(src_path):
+            shutil.copy(src_path, dst_path)
+        else:
+            copy_file_recursive(src_path, dst_path)
+
+    
+
+
 
 def main():
-    node1 = TextNode("Boot.dev",TextType.LINK,"https://www.boot.dev")
-    print(node1)
+    if os.path.exists("./public"):
+        shutil.rmtree("./public")
+    os.mkdir("./public")
+
+    copy_file_recursive("./static","./public")
 
 
-
-
-if __name__ == "__main__":
+if __name__== '__main__':
     main()
+    
+    
+
+
